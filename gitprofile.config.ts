a3c3291fee5fe5dcaad0d89840ceff41654f84c8
Md Tariqul IsIam <110732307+TariqullslamHridoy@users.noreply.github.com>
2025-10-13 01:20:00 +0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -53,8 +53,7 @@ const CONFIG = {
   },
   seo: {
     title: 'Portfolio of Md Tariqul IsIam',
-    description: 'Hey! I'm Md.Tariqul Islam (Hridoy). certified expert, in OSINT (Open Source Intelligence) with a 6+ years of experience in cybersecurity under my belt well.I excel in unearthing information reinforcing landscapes and providing thorough investigative services.I've been fortunate to work alongside teams such, where I've sharpened my expertise in analyzing cyber threats,digital forensics and conducting data inquiries. I use my expertise to offer clients the information and security they require by performing investigations or examining risks along, with furnishing
- cyber security',
+    description: 'Hey! I'm Md.Tariqul Islam (Hridoy). certified expert, in OSINT (Open Source Intelligence) with a 6+ years of experience in cybersecurity under my belt well.I excel in unearthing information reinforcing landscapes and providing thorough investigative services.
     imageURL: '',
   },
   social: {
@@ -152,7 +151,7 @@ const CONFIG = {
       institution: 'Arena Web Security',
       degree: 'CEH - Certified Ethical Hacker',
       from: '2021',
-      to: '2022',
+      to: '28-12-2022',
     },
   ],
   publications: [
